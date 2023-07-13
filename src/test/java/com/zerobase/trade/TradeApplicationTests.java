70704916ed8c09446a4af3143ebf9117f20f0381
myu7769 <myung7769@gamil.com>
2023-07-14 02:17:25 +0900

    ### 변경사항 **AS-IS** **TO-BE**  - 프로젝트 초기 설정  - README 작성

---
+++ src/test/java/com/zerobase/trade/TradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
