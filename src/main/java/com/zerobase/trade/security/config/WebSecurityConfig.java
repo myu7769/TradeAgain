cc34c26ec0f4d82a93e94a42b211cdb14e615119
myu7769 <myung7769@gamil.com>
2023-07-25 08:02:50 +0900

    chore : 패키지 이름 수정

--- src/main/java/com/zerobase/trade/security/config/WebSecurityConfig.java
+++ src/main/java/com/zerobase/trade/security/config/WebSecurityConfig.java
@@ -1,9 +1,7 @@
-package com.zerobase.trade.Security.config;
+package com.zerobase.trade.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
