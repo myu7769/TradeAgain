2b2ac83808f1d61ec7ba82358114c78966466e54
myu7769 <myung7769@gamil.com>
2023-07-25 07:35:01 +0900

    feat : password 암호화 DB 저장 chore : swagger2 추가

--- src/main/java/com/zerobase/trade/controller/SignController.java
+++ src/main/java/com/zerobase/trade/controller/SignController.java
@@ -14,9 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.zerobase.trade.exception.ErrorCode.LOGIN_CHECK_FAIL;
-
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
@@ -30,7 +27,7 @@ public class SignController {
         return ResponseEntity.ok(memberService.memberSignUp(form));
     }
 
-    @PutMapping("/signIn")
+    @PostMapping("/signIn")
     public ResponseEntity<Member> memberSignIn(@RequestBody MemberSignInForm form) {
         Member member = memberService.memberSignIn(form);
 
---
+++ src/test/java/com/zerobase/trade/Security/config/WebSecurityConfigTest.java
@@ -0,0 +1,31 @@
+package com.zerobase.trade.Security.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+class WebSecurityConfigTest {
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  @Test
+  public void EncoderAndMatch() {
+    // username에 해당하는 사용자가 이미 있는지 확인합니다.
+    //given
+    String password = "1231231313pwerwrwrr1";
+    // 비밀번호를 암호화합니다.
+    //when
+    String encodedPassword = passwordEncoder.encode(password);
+
+
+    //then
+    assertTrue(passwordEncoder.matches("1231231313pwerwrwrr1", encodedPassword));
+
+  }
+
+}
